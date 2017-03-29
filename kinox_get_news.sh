68c4ea791066b263306fd50665aba059327631b3
Bastian Bittorf <bb@npl.de>
2017-03-29 14:49:49 +0200

    parsing LINK + TITLE works now reliable

---
+++ kinox_get_news.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+ARG1="$1"		# e.g search specific entry
+URL='http://kinox.to'
+I=0
+
+# TODO: search
+# http://kinox.to/Search.html?q=Spiderwick
+# <td class="Title"><a href="/Stream/Die_Geheimnisse_der_Spiderwicks.html" onclick="return false;">Die Geheimnisse der Spiderwicks</a> <span class="Year">2008</span></td>
+
+PATTERN='<td class="Title img_preview" rel='
+{ wget -qO - "$URL" || logger -s "ERROR:$? wget '$URL'"; printf '\n%s' "$PATTERN - EOF"; } | grep ^"$PATTERN" | while read -r LINE; do {
+	LINK=
+	TITLE=
+	PARSE_TITLE=
+	[ "$LINE" = "$PATTERN - EOF" ] && logger -s "[OK] examined $I titles"
+
+	# ... <a href="/Stream/Die_Schoene_und_das_Biest_2017.html" title="Die Schöne und das Biest 2017" class=
+	# ... <a href="/Stream/Masters_of_Horror_The_Black_Cat.html" title=""Masters of Horror" The Black Cat" class=
+	for WORD in $LINE; do {
+		case "$WORD" in
+			'href="'*)
+				I=$(( I + 1 ))
+				LINK="$( echo "$WORD" | cut -d'"' -f2 )"
+			;;
+			'title="'*)
+				PARSE_TITLE='true'
+			;;
+		esac
+
+		[ -n "$PARSE_TITLE" ] && {
+			case "$WORD" in
+				'class='*)
+					PRINT='true'
+
+					[ -n "$ARG1" ] && {
+						echo "$TITLE $LINK" | grep -qi "$ARG1" || PRINT=
+					}
+
+					[ -n "$PRINT" ] && {		# TODO: why starts TITLE with a space?
+						case "$LINK" in		# TODO: cut off last char of $TITLE
+							*'.html,s'*)
+								SEASON="$( echo "$LINK" | cut -d',' -f2 )"
+								printf '%s' "# Serie: $TITLE ($SEASON) - "
+							;;
+							*)
+								printf '%s' "# $TITLE - "
+							;;
+						esac
+
+						echo "Link: ${URL}${LINK}"
+					}
+
+					break
+				;;
+				'title='*)
+					# first word
+					TITLE="$( echo "$WORD" | cut -b8- )"
+				;;
+				*)
+					TITLE="${TITLE}${TITLE:+ }${WORD%?}"	# remove last character = '"'
+				;;
+			esac
+		}
+	} done
+} done
